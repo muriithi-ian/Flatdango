a582050da9249000ea72212bc7f59f0216598f3e
Muriithi Ian <imuriithiian@gmail.com>
2022-08-30 01:04:32 +0300

    Rendered the styled movie list on nav

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Your code here
 document.addEventListener("DOMContentLoaded", () => {
 	fetchData(1).then((movie) => renderMovie(movie));
+	fetchData().then((movies) => renderNav(movies));
 });
 
 function fetchData(id = null) {
@@ -21,6 +22,7 @@ function renderMovie(movie) {
 	const ticketNumber = document.querySelector("#ticket-num");
 
 	moviePoster.src = movie.poster;
+	moviePoster.alt = movie.title;
 	movieTitle.textContent = movie.title;
 	movieRuntime.textContent = `${movie.runtime} minutes`;
 	filmInfo.textContent = movie.description;
@@ -28,7 +30,22 @@ function renderMovie(movie) {
 
 	const ticketsRemaining = movie.capacity - movie.tickets_sold;
 	ticketNumber.textContent =
-		ticketsRemaining > 0 ? ticketsRemaining : "Sold Out";
+		ticketsRemaining >= 0 ? ticketsRemaining : "Sold Out";
 }
 
-function renderNav(movies) {}
+function renderNav(movies) {
+	const navFilms = document.querySelector("#films");
+
+	//remove placeholders
+	navFilms.textContent = "";
+
+	movies.forEach((movie) => {
+		//display movie title and style
+		const ticketsRemaining = movie.capacity - movie.tickets_sold;
+		const navFilmTitle = document.createElement("li");
+		navFilmTitle.textContent = movie.title;
+		navFilmTitle.classList.add("film", "item");
+		ticketsRemaining < 1 ? navFilmTitle.classList.add("sold-out") : null;
+		navFilms.appendChild(navFilmTitle);
+	});
+}
